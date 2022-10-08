cf6d0238ee3cef0f601cf78b8b35377553f102a1
marmooo <marmooo@users.noreply.github.com>
2022-10-08 11:14:49 +0900

    bump bootstrap from 5.2.1 to 5.2.2

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,11 @@
-var CACHE_NAME = "2022-09-29 00:00";
+var CACHE_NAME = "2022-10-08 11:03";
 var urlsToCache = [
   "/irregular-verbs/",
   "/irregular-verbs/index.js",
   "/irregular-verbs/easy/",
   "/irregular-verbs/hard/",
   "/irregular-verbs/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
