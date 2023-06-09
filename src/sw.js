354050db95b44daf8f6cec604ec202ef96caa18e
marmooo <marmooo@users.noreply.github.com>
2023-06-09 11:14:38 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,10 @@
-var CACHE_NAME = "2023-04-08 17:00";
+var CACHE_NAME = "2023-06-09 11:15";
 var urlsToCache = [
   "/irregular-verbs/",
   "/irregular-verbs/index.js",
   "/irregular-verbs/easy/",
   "/irregular-verbs/hard/",
   "/irregular-verbs/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
